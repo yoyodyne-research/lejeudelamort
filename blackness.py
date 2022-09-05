1416d1ccc4f144d072ad3e1404d67b14f1b47b06
Ray Haleblian <ray@haleblian.com>
2022-09-05 16:47:11 +0000

    Fix all-off program

--- blackness.py
+++ blackness.py
@@ -1,24 +1,29 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-# alterations 2021 org.yoyodyne
-#
 # Turn off all the Neotrellis LEDs.
 
+# portions
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# 2021-2022 org.yoyodyne
+
+import os
 import sys
 import time
 
 from adafruit_blinka.board.beagleboard import beaglebone_black
 import adafruit_neotrellis.neotrellis
-import board
+from board import SCL, SDA
 import busio
 
-sys.path.append('.')
-from lejeudelamort import settings
-sys.path.pop()
-
-i2c_bus = busio.I2C(settings.SCL, settings.SDA)
-trellis = adafruit_neotrellis.neotrellis.NeoTrellis(i2c_bus)
 OFF = (0, 0, 0)
+
+if os.environ.get('PEPPER'):
+  # switch to I2C interface 1, used by Bela Pepper
+  SCL = beaglebone_black.pin.I2C1_SCL
+  SDA = beaglebone_black.pin.I2C1_SDA
+
+i2c_bus = busio.I2C(SCL, SDA)
+trellis = adafruit_neotrellis.neotrellis.NeoTrellis(i2c_bus)
 for i in range(16):
     trellis.pixels[i] = OFF
     # throttle a tad.
